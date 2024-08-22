d972497f3fd3c9877a51647c88a0951ba09552f4
gitravp <116089420+gitravp@users.noreply.github.com>
2024-08-22 14:27:58 +0530

    Update index.js
    edge case type 2

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function motion(event){
     //    + event.beta + ", "
     //    + event.gamma
     // );
-
+/*
     if(parseInt($('img').css('top'))>0 && parseInt($('img').css('top'))<(window.innerHeight-60))
     {$('img').css('top',String(parseInt($('img').css('top'))+event.beta/10)+'px')}
     else{$('img').css('top','1px')}
@@ -12,6 +12,14 @@ function motion(event){
     if(parseInt($('img').css('left'))>0 && parseInt($('img').css('left'))<(window.innerWidth-60))
         {$('img').css('left',String(parseInt($('img').css('left'))+event.gamma/10)+'px')}
     else{$('img').css('left','1px')}
+    */
+    if(parseInt($('img').css('top'))>0 && parseInt($('img').css('top'))<(window.innerHeight))
+    {$('img').css('top',String(parseInt($('img').css('top'))+event.beta/10)+'px')}
+    else{$('img').css('top',parseInt($('img').css('top'))%(window.innerHeight)+'px')}
+
+    if(parseInt($('img').css('left'))>0 && parseInt($('img').css('left'))<(window.innerWidth))
+        {$('img').css('left',String(parseInt($('img').css('left'))+event.gamma/10)+'px')}
+    else{$('img').css('left',parseInt($('img').css('left'))%(window.innerWidth)+'px')}
 
 
   }
