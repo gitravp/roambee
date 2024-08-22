f5c743b72ae54d338ec1757872d805fe877e5d15
gitravp <116089420+gitravp@users.noreply.github.com>
2024-08-22 14:32:15 +0530

    Update index.js
    actually greater than or equal to

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ function motion(event){
     //    + event.gamma
     // );
 /*
-    if(parseInt($('img').css('top'))>=0 && parseInt($('img').css('top'))<(window.innerHeight-60))
+    if(parseInt($('img').css('top'))>0 && parseInt($('img').css('top'))<(window.innerHeight-60))
     {$('img').css('top',String(parseInt($('img').css('top'))+event.beta/10)+'px')}
     else{$('img').css('top','1px')}
 
-    if(parseInt($('img').css('left'))>=0 && parseInt($('img').css('left'))<(window.innerWidth-60))
+    if(parseInt($('img').css('left'))>0 && parseInt($('img').css('left'))<(window.innerWidth-60))
         {$('img').css('left',String(parseInt($('img').css('left'))+event.gamma/10)+'px')}
     else{$('img').css('left','1px')}
     */
-    if(parseInt($('img').css('top'))>0 && parseInt($('img').css('top'))<(window.innerHeight))
+    if(parseInt($('img').css('top'))>=0 && parseInt($('img').css('top'))<(window.innerHeight))
     {$('img').css('top',String(parseInt($('img').css('top'))+event.beta/10)+'px')}
     else{$('img').css('top',parseInt($('img').css('top'))%(window.innerHeight)+'px')}
 
-    if(parseInt($('img').css('left'))>0 && parseInt($('img').css('left'))<(window.innerWidth))
+    if(parseInt($('img').css('left'))>=0 && parseInt($('img').css('left'))<(window.innerWidth))
         {$('img').css('left',String(parseInt($('img').css('left'))+event.gamma/10)+'px')}
     else{$('img').css('left',parseInt($('img').css('left'))%(window.innerWidth)+'px')}
 
