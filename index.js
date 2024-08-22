a734d55e956f835c26bc55f922f53a1589456e80
gitravp <116089420+gitravp@users.noreply.github.com>
2024-08-22 13:00:55 +0530

    Update index.js
    orientation instead of accel

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function motion(event){
     $('h2').text("Accelerometer: "
       + event.accelerationIncludingGravity.x + ", "
@@ -9,6 +10,21 @@ function motion(event){
 
 if(window.DeviceMotionEvent){
     window.addEventListener("devicemotion", motion, false);
+  }else{
+    alert("DeviceMotionEvent is not supported");
+  }
+*/
+function motion(event){
+    $('h2').text("Accelerometer: "
+      + event.alpha + ", "
+      + event.beta + ", "
+      + event.gamma
+    );
+  }
+
+
+if(window.DeviceMotionEvent){
+    window.addEventListener("deviceorientation", motion, false);
   }else{
     alert("DeviceMotionEvent is not supported");
   }
